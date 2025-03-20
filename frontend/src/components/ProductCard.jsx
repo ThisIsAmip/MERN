20b45d2c7e7cef910147e96e065e682b093e5074
ThisIsAmip <thisisamip@gmail.com>
2025-03-20 16:40:52 +0700

    Display products on Home Page.

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,16 +1,58 @@
-import { Flex, Image } from 'antd'
+import { Button, Flex, Image, Card, Typography } from 'antd'
 import React from 'react'
+const { Text, Title, Paragraph } = Typography;
 
 export const ProductCard = ({ product }) => {
+
+    //Styles
+
+    const cardStyle = {
+        width: "100%",
+        overflow: 'hidden',
+    };
+
+    //Functions, variables, etc.
+
+
     return (
         <>
-            <Flex>
-                <Image
-                    width={200}
-                    src="https://images.unsplash.com/photo-1523170335258-f5ed11844a49?q=80&w=2380&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                />
-                <h1>Product 1</h1>
-            </Flex>
+            <Card
+                style={cardStyle}
+                styles={{
+                    body: {
+                        padding: 0,
+                    },
+                }}
+            >
+                <Flex
+                    vertical
+                    justify='center'
+                    align='center'
+                >
+                    <Image
+                        style={{ width: '100%' }}
+                        src={product.imageUrl}
+                        alt={product.name}
+                    />
+                    <Flex vertical style={{ padding: '1em', width: "100%" }}>
+                        <Typography.Title level={3} style={{ margin: "0px 0px 10px 0px" }}>
+                            {product.name}
+                        </Typography.Title>
+                        <Paragraph ellipsis={{ rows: 2 }} style={{ width: "100%", fontSize: '1.2em', }}>
+                            {product.description}
+                        </Paragraph>
+
+                        <Flex justify='space-between' align='center' style={{ width: '100%' }} >
+                            <Text strong >Price: {product.price}</Text>
+                            <Text strong >In-stock: {product.countInStock}</Text>
+                            <Flex gap='small'>
+                                <Button color="primary" variant="outlined">View & Edit</Button>
+                                <Button color="danger" variant="solid">Delete</Button>
+                            </Flex>
+                        </Flex>
+                    </Flex>
+                </Flex>
+            </Card>
         </>
     )
 }
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,14 +1,29 @@
-import { Col, Row, Flex, Image } from 'antd';
+import { Col, Row, Flex, Button, Image, Card, Typography } from 'antd';
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import { useProductStore } from '../store/product';
+import { ProductCard } from '../components/ProductCard';
+const { Text, Title, Paragraph } = Typography;
 
 const HomePage = () => {
 
+  //Styles
+
+  const cardStyle = {
+    width: "100%",
+    overflow: 'hidden',
+  };
+
+  //Functions, variables, etc.
+
   const { fetchProducts, products } = useProductStore();
   useEffect(() => {
-    fetchProducts();
-
+    const fetchData = async () => {
+      console.log('Fetching products...');
+      await fetchProducts();
+      console.log('Products after fetch:', products);
+    };
+    fetchData();
   }, [fetchProducts]);
   console.log(products);
 
@@ -21,21 +36,22 @@ const HomePage = () => {
         style={{ height: '100vh', marginTop: '5em' }}
         vertical
       >
+        {products.length === 0 && (
+          <h2 >No Products found ;-; {" "}
+            <Link to="/create">
+              <span >Create a Product</span>
+            </Link>
+          </h2>
+        )}
         <div
 
           style={{
             width: '100%',
             maxWidth: '600px',
-            padding: '2em',
 
           }}>
-          <h1 style={{ color: 'black', textAlign: 'center', }}>Home Page</h1>
+          <Title style={{textAlign: 'center', margin: 0}}>Home Page</Title>
         </div>
-        <h2 >No Products found ;-; {" "}
-          <Link to="/create">
-            <span >Create a Product</span>
-          </Link>
-        </h2>
         <Row
           justify={'start'}
           gutter={[16, 16]}
@@ -44,31 +60,54 @@ const HomePage = () => {
             padding: '2em',
           }}
         >
+          {products.map((product) => (
+            <Col
+              lg={6} md={8} sm={12} xs={24}
+            >
+              <ProductCard key={product._id} product={product} />
+            </Col>
+          ))}
 
 
-          <Col lg={6} md={8} sm={12} xs={24}
-          style={{
-            overflow: 'hidden',
-          }}
-          >
-            <Flex
-              vertical
-              justify='center'
-              align='center'
-              style={{
-                backgroundColor: 'white',
+          {/* <Card
 
-                borderRadius: '10px'
-              }}>
-              <Image
-                style={{width: '100%'}}
-                src="https://images.unsplash.com/photo-1523170335258-f5ed11844a49?q=80&w=2380&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              />
-              <h1>Product 1</h1>
-              <p style={{ fontSize: '1.2em' }}>Product 1</p>
+              style={cardStyle}
+              styles={{
+                body: {
+                  padding: 0,
+                },
+              }}
+            >
+              <Flex
+                vertical
+                justify='center'
+                align='center'
+              >
+                <Image
+                  style={{ width: '100%', }}
+                  src="https://images.unsplash.com/photo-1523170335258-f5ed11844a49?q=80&w=2380&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                />
+                <Flex vertical style={{ padding: '1em' }}>
+                  <Title level={3} style={{ margin: "0px 0px 10px 0px" }}>
+                    A Very Long Product Name
+                  </Title>
+                  <Paragraph ellipsis={{ rows: 2 }} style={{ width: "100%", fontSize: '1.2em' }}>
+                    Lorem ipsum dolor, sit amet adipisicing elit. Non quisquam reiciendis voluptatem accusantium, incidunt distinctio debitis labore, vero sed placeat, iste dignissimos quas animi molestiae beatae dolor adipisci fuga quidem?
+                  </Paragraph>
 
-            </Flex>
-          </Col>
+                  <Flex justify='space-between' align='center' style={{ width: '100%' }} >
+                    <Text strong >Price: $10000000</Text>
+                    <Text strong >In-stock: 10000000</Text>
+                    <Flex gap='small'>
+                      <Button color="primary" variant="outlined">View & Edit</Button>
+                      <Button color="danger" variant="solid">Delete</Button>
+                    </Flex>
+                  </Flex>
+                </Flex>
+
+              </Flex>
+            </Card> */}
+
 
 
         </Row>
